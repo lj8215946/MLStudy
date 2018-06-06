7a99ae3cd3a44ac112e8cc8de8880d29dd43092c
connorlu <luji90@126.com>
2018-06-06 20:19:10 +0800

    style(module QLearningWithTF): 调整了一些变量的命名。

--- RL/QLeaningWithTF.py
+++ RL/QLeaningWithTF.py
@@ -36,9 +36,11 @@ with tf.Session() as sess:
         # Reset environment and get first new observation
         s = env.reset()
         rAll = 0
-        d = False
+        done = False
         # The Q-Network
         j = 0
+        if i == num_episodes - 1:
+            env.render()
         for j in range(99):
             # Choose an action by greedily (with e chance of random action) from the Q-network
             # 这里的np.identity(16)[s:s+1]的这个是用过单位矩阵生成了one hot编码
@@ -47,18 +49,20 @@ with tf.Session() as sess:
             if np.random.rand(1) < e:
                 a[0] = env.action_space.sample()
             # Get new state and reward from environment
-            s1, r, d, _ = env.step(a[0])
+            new_state, reward, done, _ = env.step(a[0])
+            if i == num_episodes - 1:
+                env.render()
             # Obtain the Q' values by feeding the new state through our network
-            Q1 = sess.run(Qout, feed_dict={inputs1: np.identity(16)[s1:s1+1]})
+            Q1 = sess.run(Qout, feed_dict={inputs1: np.identity(16)[new_state:new_state + 1]})
             # Obtain maxQ' and set our target value for chosen action.
             maxQ1 = np.max(Q1)
             targetQ = allQ
-            targetQ[0, a[0]] = r + y*maxQ1
+            targetQ[0, a[0]] = reward + y * maxQ1
             # Train our network using target and predicted Q values
             _, W1 = sess.run([updateModel, W], feed_dict={inputs1: np.identity(16)[s:s+1], nextQ: targetQ})
-            rAll += r
-            s = s1
-            if d:
+            rAll += reward
+            s = new_state
+            if done:
                 # Reduce chance of random action as we train the model.
                 e = 1./((i/50) + 10)
                 break
