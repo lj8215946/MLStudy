4708a74124b8ed699de942f48519ebdbcc44e664
connorlu <connorlu@tencent.com>
2018-06-08 16:01:11 +0800

    feat(module ContextualBandit): 提交有状态的老虎机的训练代码。

---
+++ RL/ContextualBandit_policyGradient.py
@@ -0,0 +1,105 @@
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import numpy as np
+
+
+class ContextualBandit:
+    def __init__(self):
+        self.state = 0
+        # List out our bandits. Currently arms 4, 2, and 1 (respectively) are the most optimal.
+        self.bandits = np.array([[0.2, 0, -0.0, -5], [0.1, -5, 1, 0.25], [-5, 5, 5, 5]])
+        self.num_bandits = self.bandits.shape[0]
+        self.num_actions = self.bandits.shape[1]
+
+    def get_bandit(self):
+        self.state = np.random.randint(0, len(self.bandits))  # Returns a random state for each episode.
+        return self.state
+
+    def pull_arm(self, user_action):
+        # Get a random number.
+        bandit = self.bandits[self.state, user_action]
+        result = np.random.randn(1)
+        if result > bandit:
+            # return a positive reward.
+            return 1
+        else:
+            # return a negative reward.
+            return -1
+
+
+class Agent:
+    def __init__(self, lr, s_size, a_size):
+        # These lines established the feed-forward part of the network. The agent takes a state and produces an action.
+        self.state_in = tf.placeholder(shape=[1], dtype=tf.int32)
+        state_in_one_hot = slim.one_hot_encoding(self.state_in, s_size)
+        output = slim.fully_connected(state_in_one_hot, a_size,
+                                      biases_initializer=None, activation_fn=tf.nn.sigmoid,
+                                      weights_initializer=tf.ones_initializer())
+        self.output = tf.reshape(output, [-1])
+        self.chosen_action = tf.argmax(self.output, 0)
+
+        # The next six lines establish the training proceedure. We feed the reward and chosen action into the network
+        # to compute the loss, and use it to update the network.
+        self.reward_holder = tf.placeholder(shape=[1], dtype=tf.float32)
+        self.action_holder = tf.placeholder(shape=[1], dtype=tf.int32)
+        self.responsible_output = tf.slice(self.output, self.action_holder, [1])
+
+        # 使用policy gradient方法进行训练
+        # 实际上就是根据action的回报去奖励或者惩罚output
+        # 这里只要奖励是正的就会奖励RL选中的哪个output，否则就会惩罚RL选中的output
+        # 最终TF会通过跳转weight值达到上述目的
+        self.loss = -(tf.log(self.responsible_output) * self.reward_holder)
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=lr)
+        self.update = optimizer.minimize(self.loss)
+
+
+# 开始训练
+
+tf.reset_default_graph()  # Clear the Tensorflow graph.
+
+cBandit = ContextualBandit()  # Load the bandits.
+myAgent = Agent(lr=0.001, s_size=cBandit.num_bandits, a_size=cBandit.num_actions)  # Load the agent.
+weights = tf.trainable_variables()[0]  # The weights we will evaluate to look into the network.
+
+total_episodes = 10000  # Set total number of episodes to train agent on.
+total_reward = np.zeros([cBandit.num_bandits, cBandit.num_actions])  # Set scoreboard for bandits to 0.
+e = 0.1  # Set the chance of taking a random action.
+
+init = tf.global_variables_initializer()
+
+# Launch the tensorflow graph
+with tf.Session() as sess:
+    sess.run(init)
+    i = 0
+    while i < total_episodes:
+        s = cBandit.get_bandit()  # Get a state from the environment.
+
+        # Choose either a random action or one from our network.
+        if np.random.rand(1) < e:
+            action = np.random.randint(cBandit.num_actions)
+        else:
+            action = sess.run(myAgent.chosen_action, feed_dict={myAgent.state_in: [s]})
+
+        reward = cBandit.pull_arm(action)  # Get our reward for taking an action given a bandit.
+
+        # Update the network.
+        feed_dict = {myAgent.reward_holder: [reward], myAgent.action_holder: [action], myAgent.state_in: [s]}
+        _, ww = sess.run([myAgent.update, weights], feed_dict=feed_dict)
+
+        # Update our running tally of scores.
+        total_reward[s, action] += reward
+        if i % 500 == 0:
+            print("Mean reward for each of the " + str(cBandit.num_bandits) + " bandits: "
+                  + str(np.mean(total_reward, axis=1)))
+        i += 1
+
+for a in range(cBandit.num_bandits):
+
+    print("The agent thinks action " + str(np.argmax(ww[a]) + 1) + " for bandit " + str(a + 1)
+          + " is the most promising....")
+
+    if np.argmax(ww[a]) == np.argmin(cBandit.bandits[a]):
+        print("...and it was right!")
+    else:
+        print("...and it was wrong!")
+
